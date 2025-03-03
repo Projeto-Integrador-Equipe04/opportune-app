5987beea4c099111d8a30cc5405d6f29d946805a
Lucas Manhães <ramosmanhaes@outlook.com>
2025-03-03 15:24:27 -0300

    Edit: Alterando disposição de navbar e demais componentes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(){
     
     return(
         <>
-            <footer className="relative -z-10 left-20 ml-1 w-full flex justify-center p-10 bg-[#99B8BA]">
+            <footer className="absolute -z-10 top-0 left-20 w-[calc(100%-5rem)] h-[101px] bg-[#99B8BA] text-white text-lg flex justify-center items-center px-6">
                 <p>Todos os direitos reservados | Copyright: {data}</p>
             </footer>
         </>
