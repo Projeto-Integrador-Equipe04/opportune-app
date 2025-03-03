8d350fdde40729fbf358178b126e58dbb38551d8
Lucas Manhães <ramosmanhaes@outlook.com>
2025-03-03 11:11:31 -0300

    Add: host e porta, server do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()   
+    tailwindcss(),
   ],
+  server:{
+    host: "0.0.0.0",
+    port: 3000,
+    open: true
+  }
 })
