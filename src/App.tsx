9fd71856444790cc7c39aa2eae95d47c9b270e84
Beatriz Santina <beatriizsantina14@gmail.com>
2025-03-05 13:10:28 -0300

    Adicionando rota perfil-empresa

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Navbar from "./components/navbar/Navbar";
+import { Home } from "lucide-react";
 import CadastroCliente from "./components/cadastroCliente/CadastroCliente";
+import Dashboard from "./pages/dashboard/Dashboard";
 import CadastroPlano from "./components/cadastroPlano/CadastroPlano";
+import Loginempresa from "./pages/loginempresa/Loginempresa";
+import Cadastroempresa from "./pages/cadastroempresa/CadastroEmpresa";
+import Planos from "./pages/plano/Planos";
+import Clientes from "./pages/cliente/Cliente";
 import PerfilEmpresa from "./pages/perfil/PerfilEmpresa";
+import Footer from "./components/footer/Footer";
 
-function App() {
+
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/cadastro-cliente" element={<CadastroCliente />} />
-        <Route path="/cadastro-plano" element={<CadastroPlano />} />
-        <Route path="/perfil-empresa" element={<PerfilEmpresa />} />
-      </Routes>
-    </Router>
-  );
-}
+    <>
+      <BrowserRouter>
+        <AuthProvider> 
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path="/cadastro-cliente" element={<CadastroCliente />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path="/cadastro-plano" element={<CadastroPlano />} />
+            <Route path="/login" element={<Loginempresa />} />
+            <Route path="/cadastroempresa" element={<Cadastroempresa />} />
+            <Route path="/planos" element={<Planos />} />
+            <Route path="/clientes" element={<Clientes/>} />
+            <Route path="/perfil-empresa" element={<PerfilEmpresa />} />
+
+          </Routes>
+          <Footer />
+        </AuthProvider>
+      </BrowserRouter>
+    </>
+  );}
