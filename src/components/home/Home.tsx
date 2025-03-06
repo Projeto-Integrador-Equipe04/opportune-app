a246dbcefb08b1e3041a9a72b76004e176f408ae
Lucas Ramos <lucas@MacBook-Pro-de-Lucas.local>
2025-03-06 14:32:51 -0800

    Fix: bug das imagens

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 
+import logo from "../../../public/image/Logo.svg"
+import home from "../../../public/image/bg-home.jpg"
+
 export default function Home(){
     return(
         <>
         <main className="grid grid-cols-1 lg:grid-cols-2 h-screen w-full">
-            <div className="relative flex items-center justify-center bg-[url(public/image/bg-home.jpg)] bg-no-repeat bg-cover">
+            <div className="relative flex items-center justify-center bg-[url(${home})] bg-no-repeat bg-cover">
                 <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
             </div>
             <div className="flex flex-col justify-center">
                 <div className="p-10 flex flex-row">
                     <div>
-                        <img className="w-72" src="public/image/Logo.svg" alt="" />
+                        <img className="w-72" src={logo} alt="" />
                         <p className="text-[#4D4B4B] font-semibold text-[16px] mx-4 opacity-54 font-sans mt-0">O melhor CRM para seu negócio! </p>
                     </div>
                 </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,6 +3,14 @@ import "./Navbar.css"
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
+import eye from "../../../public/icons/eye.svg"
+import home from "../../../public/icons/home.svg"
+import person from "../../../public/icons/person2.svg"
+import hand from "../../../public/icons/hand.svg"
+import building from "../../../public/icons/building.svg"
+import coffee from "../../../public/icons/coffee.svg"
+import exit from "../../../public/icons/exit.svg"
+
 export default function Navbar(){
     const { empresa, handleLogout } = useContext(AuthContext)
     const token = empresa?.token;
@@ -14,33 +22,33 @@ export default function Navbar(){
             <nav className="fixed z-[9999] inset-y-0 left-0 bg-[#EEF6F5] nav">
                 <div className="h-full flex flex-col justify-between items-center py-8 px-3">
                     <Link to={"/dashboard"} className="flex items-end hover:decoration-transparent">
-                        <img src="./public/icons/eye.svg" className="w-7"/>
+                        <img src={eye} className="w-7"/>
                         <h1 className="text-2xl item-close text-black">pportune</h1>
                     </Link>
                     <div className="flex flex-col gap-6">
                         <Link to={"/dashboard"} className="flex items-end gap-1 p-4 text-black hover:bg-[#99B8BA] hover:rounded-lg hover:text-black">
-                            <img src="./public/icons/home.svg" className="w-7"/>
+                            <img src={home} className="w-7"/>
                             <p className="item-close">Dashboard</p>
                         </Link>
                         <Link to={"/clientes"} className="flex items-end gap-1 p-4 text-black hover:bg-[#99B8BA] hover:rounded-lg hover:text-black">
-                            <img src="./public/icons/person2.svg" className="w-7"/>
+                            <img src={person} className="w-7"/>
                             <p className="item-close">Clientes</p>
                         </Link>
                         <Link to={"/planos"} className="flex items-end gap-1 p-4 text-black hover:bg-[#99B8BA] hover:rounded-lg hover:text-black">
-                            <img src="./public/icons/hand.svg" className="w-6"/>
+                            <img src={hand} className="w-6"/>
                             <p className="item-close">Planos</p>
                         </Link>
                         <Link to={"/perfil-empresa"} className="flex items-end gap-1 p-4 text-black hover:bg-[#99B8BA] hover:rounded-lg hover:text-black">
-                            <img src="./public/icons/building.svg" className="w-6"/>
+                            <img src={building} className="w-6"/>
                             <p className="item-close">Empresa</p>
                         </Link>
                         <Link to={"/sobre"} className="flex items-end gap-1 p-4 text-black hover:bg-[#99B8BA] hover:rounded-lg hover:text-black">
-                            <img src="./public/icons/coffee.svg" className="w-6"/>
+                            <img src={coffee} className="w-6"/>
                             <p className="item-close">Sobre</p>
                         </Link>
                     </div>
                     <Link to={"/login"}  onClick={handleLogout} className="flex items-end gap-1 py-4 text-black px-8 hover:bg-[#99B8BA] hover:rounded-lg">
-                        <img src="./public/icons/exit.svg" className="w-6"/>
+                        <img src={exit} className="w-6"/>
                         <p className="item-close">Sair</p>
                     </Link>
                 </div>
